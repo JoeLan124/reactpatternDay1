1b2202d3de852e8da7f8448611ca87ec07cf951d
Johannes Langosch <johannes.langosch@googlemail.com>
2025-10-11 16:56:41 +0200

    second try

--- solution3/app/components/Card.jsx
+++ solution3/app/components/Card.jsx
@@ -1,28 +1,42 @@
-const Card = ({ children}) => {
+function Card ({ children})  {
   return (
-      <div className="bg-gray-300 rounded-2xl backdrop-blur-3xl text-blue-700 h-34 w-1/2">
+      <div className="bg-white rounded-2xl  text-blue-700 h-34 w-1/2">
           {children}
     </div>
   )
 }
-const CardHeader = ({title, children}) => {
-    return <div>
-        <h1>{title }</h1>
-        {children}</div>
+
+function CardHeader({ title, children }) {
+    return (
+      <div>
+        <h1>{title}</h1>
+        {children}
+      </div>
+    );
 }
 
-const CardBody = ({ children}) => {
-    return <div>
-        {children}</div>
+function CardBody ({ children})  {
+    return (
+        <div>
+            {children}
+        </div>
+    )
 }
 
-const CardFooter = ({children }) => {
-    return <div>
-        {children}</div>;
+function CardFooter ({children })  {
+    return (
+        <div>
+            {children}
+        </div>
+    
+    )
 };
 
 Card.Header = CardHeader;
 Card.Body = CardBody;
 Card.Footer = CardFooter;
+
+// Attach subcomponents
+
 
 export default Card
