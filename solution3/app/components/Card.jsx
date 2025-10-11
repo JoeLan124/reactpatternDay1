535df22b40a86e3ae4beb5e5d0bb4ff43fda34d1
Johannes Langosch <johannes.langosch@googlemail.com>
2025-10-11 17:25:28 +0200

    fine tuning 2

--- solution3/app/components/Card.jsx
+++ solution3/app/components/Card.jsx
@@ -1,4 +1,4 @@
-function Card({ children }) {
+const Card = ({ children })=> {
   return (
     <div className="bg-white text-black rounded-2xl p-6">
       {children}
@@ -6,10 +6,8 @@ function Card({ children }) {
   );
 }
 
- 
 
-
-function CardHeader({ title, children, className }) {
+const CardHeader = ({ title, children, className }) => {
     return (
         <div className={`mb-4 border-1 border-gray-100 rounded-3xl p-4 shadow-2xl ${className}`} >
           <h1 className="text-xl font-bold">{title}</h1>
@@ -22,12 +20,12 @@ function CardHeader({ title, children, className }) {
 
 
 
-function CardBody({ children }) {
+const CardBody = ({ children }) =>{
   return <div className="mb-4 pl-6">{children}</div>;
 }
 
 
-function CardFooter ({children })  {
+const CardFooter =  ({children }) => {
     return (
       <div className="mb-4 pl-6">
         <hr className="my-4 text-blue-100" />
