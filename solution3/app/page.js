a031d1c1b12a5b9d584acad730d0b11adfd6092a
Johannes Langosch <johannes.langosch@googlemail.com>
2025-10-26 14:49:11 +0100

    Accordion

--- solution3/app/page.js
+++ solution3/app/page.js
@@ -2,7 +2,8 @@ import MessyImageCardDemo from "./components/(MessyImageCards)/MesseImageCardDem
 import CleanCardDemo1 from "./components/(CleanCard1)/CleanImageCard1Demo";
 import CleanCardDemo2 from "./components/(CleanCard2)/CleanImageCard2Demo"
 import TabDemo from "./components/(Tabs)/TabDemo";
+import AccordionDemo from "./components/(Accordion)/AccordionDemo";
 
 export default function Home() {
-  return <TabDemo/>
+  return <AccordionDemo/>
 }
