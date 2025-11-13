fd9e9b6b5ba21c6243a289dcf67684c326c9927e
Johannes Langosch <johannes.langosch@googlemail.com>
2025-11-13 20:10:45 +0100

    show user in page

--- solution7/app/page.tsx
+++ solution7/app/page.tsx
@@ -1,14 +1,16 @@
 
+"use client"
 import Navbar from "./components/Navbar";
+import { useAuth } from "@/app/hooks/useAuth";
 
 export default function Home() {
-
+  const { isAuthenticated, login } = useAuth();
 
 
   return (
     <div className="min-h-screen justify-center bg-zinc-50 font-sans dark:bg-black text-black">
       <Navbar />
-      <p className="p-6 text-red-700">Hallo</p>
+      <p className="p-6 text-red-700">{ isAuthenticated? "User John": "please log in"}</p>
     </div>
   );
 }
