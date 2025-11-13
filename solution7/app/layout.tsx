4ad5ad287bc91357a0b80d4d53fe9d405416201f
Johannes Langosch <johannes.langosch@googlemail.com>
2025-11-13 20:08:45 +0100

    Provider now wrapped

--- solution7/app/layout.tsx
+++ solution7/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
+import AuthProvider from "@/app/provider/AuthProvider"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,8 +27,11 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-      
+        <AuthProvider>
+
+
         {children}
+        </AuthProvider>
       </body>
     </html>
   );
