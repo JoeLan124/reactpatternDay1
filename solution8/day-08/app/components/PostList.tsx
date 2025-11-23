54e197a54eeb2269f5552564d1ea70c1d6837443
Johannes Langosch <johannes.langosch@googlemail.com>
2025-11-23 17:22:12 +0100

    only post title

--- solution8/day-08/app/components/PostList.tsx
+++ solution8/day-08/app/components/PostList.tsx
@@ -3,11 +3,11 @@ import type { Post } from "../page";
 const PostList = ({ posts }:{posts: Post[]}) => {
   return (
     <main>
-      <h1>Meine Blog PostList</h1>
+
       {posts.map((post) => (
         <article key={post.id}>
           <h2>{post.title}</h2>
-          <p>{post.content}</p>
+          {/* <p>{post.content}</p> */}
         </article>
       ))}
     </main>
