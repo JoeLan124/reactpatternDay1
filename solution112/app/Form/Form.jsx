86908c1911a18acf283dc6ddd8af2f2a7ceda59c
Johannes Langosch <johannes.langosch@googlemail.com>
2026-02-08 10:08:05 +0100

    lazy country selector in form

---
+++ solution112/app/Form/Form.jsx
@@ -0,0 +1,78 @@
+"use client"
+import React, { useActionState, useState } from "react"
+
+const CountrySelectorLazy = React.lazy(() => import("../components/CountrySelector/page"))
+
+
+const Form = () => {
+    const [selectedCountry, setSelectedCountry] = useState("")
+    const [state, formAction, isPending] = useActionState(
+      updateInputData,
+      {
+        error: null,
+        success: false,
+      },
+    );
+  
+  async function updateInputData(prevState, formData) {
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const country = formData.get("country");
+
+    // Simulierte API-Verzögerung
+    await new Promise((res) => setTimeout(res, 1000));
+
+
+    return {
+      error: null,
+      success: true,
+      message: `Daten erfolgreich geändert!`,
+    };
+  }
+  
+  
+    return (
+      <form action="formAction">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            disabled={isPending}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            disabled={isPending}
+          />
+        </div>
+
+        <div className="space-y-4">
+          <h2 className="text-xl font-semibold">
+            Country Selector
+          </h2>
+          <CountrySelectorLazy
+            onCountryChange={setSelectedCountry}
+          />
+          <button type="submit" disabled={isPending}>
+            {isPending
+              ? "Wird gespeichert..."
+              : "Speichern"}
+          </button>
+        </div>
+        {/* 3. Feedback an den User basierend auf dem State */}
+        {state.error && (
+          <p style={{ color: "red" }}>{state.error}</p>
+        )}
+        {state.success && (
+          <p style={{ color: "green" }}>{state.message}</p>
+        )}
+      </form>
+    );
+}
+export default Form
--- solution112/app/page.tsx
+++ solution112/app/page.tsx
@@ -3,25 +3,16 @@
 import { useState } from "react"
 import Memoization from "./components/BrokenMemoization/page"
 import ProductCard from "./components/ProductCard"
-import CountrySelector from "./components/CountrySelector/page"
+import Form from "./Form/Form"
 
 export default function Home() {
-  const [selectedCountry, setSelectedCountry] = useState<string>("")
+
 
   return (
     <div className="p-8 space-y-8">
       <Memoization />
       <ProductCard price={12} />
-      
-      <div className="space-y-4">
-        <h2 className="text-xl font-semibold">Country Selector</h2>
-        <CountrySelector onCountryChange={setSelectedCountry} />
-        {selectedCountry && (
-          <p className="text-sm text-muted-foreground">
-            Selected country: <strong>{selectedCountry}</strong>
-          </p>
-        )}
-      </div>
+      <Form/>
     </div>
   )
 }
